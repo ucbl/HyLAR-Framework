bac4e2fe7ac17dba9795c20c84d3092cc813ce8e
Spadon <m.terdjimi@gmail.com>
2015-02-01 16:01:54 +0100

    Added decentralized message processing to avoid importScripts when not into a worker

---
+++ app/workers/ReasonerReceive.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Spadon on 30/01/2015.
+ */
+
+function receive(event) {
+    var data = JSON.parse(event.data);
+    if(data.command === "start") {
+        return startReasoner(data);
+    } else if(data.command === "process") {
+        return queryReasoner(data.sparqlQuery, data.reasoner, data.inWorker);
+    }
+}
