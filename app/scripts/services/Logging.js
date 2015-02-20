29ff516d06726caef442decbca28936f347c57eb
Spadon <m.terdjimi@gmail.com>
2015-02-20 11:05:44 +0100

    Service short refactoring

--- app/scripts/services/Logging.js
+++ app/scripts/services/Logging.js
@@ -1,3 +1,29 @@
 /**
  * Created by Spadon on 20/02/2015.
  */
+
+app.service('LoggingService', function() {
+
+    var msgData;
+
+    this.log = [];
+
+    this.msg = function(content) {
+        msgData = new Object();
+        msgData.msg = content;
+        msgData.isError = false;
+        return this;
+    };
+
+    this.err = function(content) {
+        msgData = new Object();
+        msgData.msg = content;
+        msgData.isError = true;
+        return this;
+    };
+
+    this.submit = function() {
+        msgData.time = new Date().getTime();
+        this.log.push(msgData);
+    }
+});
