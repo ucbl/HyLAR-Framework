76ce5b954f4697b6a45a0b38d10c5aa6fbfb1cfe
Mehdi Terdjimi <mehdi.terdjimi@univ-lyon1.fr>
2015-07-21 18:11:06 +0200

    minor modif

--- server/utils/Utils.js
+++ server/utils/Utils.js
@@ -19,7 +19,7 @@ module.exports = {
      * @param res
      */
     allowCrossDomain: function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+        res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type');
         next();
