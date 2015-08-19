c84bea16045d217c0cb055b36f59bf512a8d32ba
Spadon <m.terdjimi@gmail.com>
2015-08-19 10:51:16 +0200

    Classification test doesnt pass.

--- test/test.js
+++ test/test.js
@@ -4,51 +4,45 @@
 
 var should = require('should');
 var fs = require('fs');
+var path = require('path');
 
 var JswParser = require('../server/ontology/jsw/JswParser');
 var JswBrandT = require('../server/ontology/jsw/JswBrandT');
 
-describe('Parsing and Classification on complex ontology (fipa)', function () {
-    it('should have at least 1 ClassAssertion and 1 ObjectPropertyAssertion', function () {
-        var owl, ontology, reasoner, fipa = '../server/ontologies/fipa.owl';
+var owl, ontology, reasoner, fipa = '/../server/ontologies/fipa.owl';
 
-        describe('File access', function () {
-            it('should access the file', function () {
-                fs.exists(fipa, function (fileok) {
-                    fileok.should.equal(true);
-                });
-            });
+describe('File access', function () {
+    it('should access the file', function () {
+        var exists = fs.existsSync(path.resolve(__dirname + fipa));
+        exists.should.equal(true);
+    });
+});
+
+describe('File reading', function () {
+    it('should correctly read the file', function () {
+        var data = fs.readFileSync(path.resolve(__dirname + fipa));
+        data.should.exist;
+        owl = data.toString().replace(/(&)([a-z0-9]+)(;)/gi, '$2:');
+    });
+});
+
+describe('Ontology parsing', function () {
+    it('should parse the ontology', function () {
+        ontology = JswParser.parse(owl, function (err) {
+            console.err(err);
         });
+        ontology.should.exist;
+    });
+});
 
-        describe('File reading', function () {
-            it('should correctly read the file', function () {
-                fs.readFile(fipa, function (err, data) {
-                    if (err) {
-                        console.err(err.toString());
-                    } else {
-                        owl = data.toString().replace(/(&)([a-z0-9]+)(;)/gi, '$2:');
-                    }
-                });
-                should.exist(owl);
-            });
-        });
+describe('Ontology Classification', function () {
+    it('should classify the ontology', function () {
+        reasoner = JswBrandT.reasoner(ontology);
+        reasoner.should.exist;
+    });
 
-        describe('Ontology parsing', function () {
-            it('should parse the ontology', function () {
-                ontology = JswParser.parse(owl, function (err) {
-                    console.err(err);
-                });
-                should.exist(ontology);
-            });
-        });
-
-        describe('Ontology Classification', function () {
-            it('should classify the ontology', function () {
-                reasoner = JswBrandT.reasoner(ontology);
-                reasoner.should.exist;
-            });
-        });
-
-        reasoner.ontology.should.exist;
+    it('should find some Classes and ObjectProperties', function() {
+       reasoner.aBox.database.ClassAssertion.should.be.above(0);
+       reasoner.aBox.database.ObjectPropertyAssertion.should.be.above(0);
     });
 });
